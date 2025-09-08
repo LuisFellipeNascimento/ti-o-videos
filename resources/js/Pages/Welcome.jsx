6a513c854f15adbff12161462cd0116ae902039c
LuisFellipeNascimento <112833744+LuisFellipeNascimento@users.noreply.github.com>
2025-09-08 13:07:36 -0300

    Adicionando o footer

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -174,7 +174,7 @@ export default function Welcome({
                         </main>
 
                         <footer className="py-16 text-center text-sm text-black dark:text-white/70">
-                            Laravel v{laravelVersion} (PHP v{phpVersion})
+                            Desenvolvido por Luis Fellipe com Laravel v{laravelVersion} (PHP v{phpVersion})
                         </footer>
                     </div>
                 </div>
